e91c19e99fc60e62da1b65646e7e9cbdf3eb4e6c
Surag <surag.noreply@gmail.com>
2023-07-19 16:55:06 +0000

    try 2

--- app.go
+++ app.go
@@ -26,13 +26,13 @@ func main() {
 	req := texttospeechpb.SynthesizeSpeechRequest{
 		// Set the text input to be synthesized.
 		Input: &texttospeechpb.SynthesisInput{
-			InputSource: &texttospeechpb.SynthesisInput_Text{Text: "Hello, World!"},
+			InputSource: &texttospeechpb.SynthesisInput_Text{Text: "I can't deny the fire that still burns between us, the way our souls used to intertwine. We had something electric, something rare. And I know we've both moved on, but I can't help but wonder if deep down, you feel it too. The love we shared was passionate, intense, and I can't shake the feeling that we left something unfinished. Maybe it's time we revisit those uncharted territories, those depths of passion we never truly explored. Because when I look into your eyes, I see a reflection of the love that once consumed us, and I'm willing to risk it all to see if we can find our way back to each other."},
 		},
 		// Build the voice request, select the language code ("en-US") and the SSML
 		// voice gender ("neutral").
 		Voice: &texttospeechpb.VoiceSelectionParams{
 			LanguageCode: "en-US",
-			SsmlGender:   texttospeechpb.SsmlVoiceGender_NEUTRAL,
+			SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
 		},
 		// Select the type of audio file you want returned.
 		AudioConfig: &texttospeechpb.AudioConfig{
